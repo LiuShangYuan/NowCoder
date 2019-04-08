985dc8b916b55a380849f10a71e39de1daa09598
yinkai <2961709898@qq.com>
2019-04-08 20:51:43 +0800

    有序二维数组查找元素

--- v1/src/cn/scut/SearchInTwoDimensionalArray.java
+++ v1/src/cn/scut/SearchInTwoDimensionalArray.java
@@ -1,4 +1,45 @@
 package cn.scut;
 
+/**
+ * 在有序的二位数组里面查找某个数
+ */
 public class SearchInTwoDimensionalArray {
+
+
+    /**
+     * 从右上角的元素考虑
+     *
+     * @param target
+     * @param array
+     * @return
+     */
+    public static boolean Find(int target, int[][] array) {
+
+        if (array[0].length == 0) {
+            return false;
+        }
+
+        int x = array[0].length - 1;
+        int y = 0;
+
+
+        while (x>=0 && y<array.length) {
+            if (target == array[y][x]) {
+                return true;
+            } else if (target > array[y][x]) {
+                y++;
+            } else {
+                x--;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {2, 3, 4}, {3, 4, 5}, {4, 5, 6}};
+//        int[][] array = {{1, 3 ,5, 7 ,9, 11}};
+        System.out.println(Find(4, array));
+    }
 }
