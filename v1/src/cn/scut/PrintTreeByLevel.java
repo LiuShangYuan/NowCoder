902fc206884cc7da33030eeab28b0b6d8e703e50
yinkai <2961709898@qq.com>
2019-05-12 22:35:35 +0800

    print tree by level

--- v1/src/cn/scut/PrintTreeByLevel.java
+++ v1/src/cn/scut/PrintTreeByLevel.java
@@ -1,4 +1,40 @@
 package cn.scut;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+
 public class PrintTreeByLevel {
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            TreeNode node = queue.remove();
+            list.add(node.val);
+
+            if(node.left != null) {
+                queue.add(node.left);
+            }
+            if(node.right != null) {
+                queue.add(node.right);
+            }
+        }
+
+
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
